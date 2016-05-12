2a1fe750a18d0b6aac4bead2247e7a2459ab696a
Priyatam Mudivarti <priyatam@gmail.com>
2016-05-11 20:32:07 -0700

    updated deps, cleanup

--- project.clj
+++ project.clj
@@ -1,29 +1,28 @@
-(defproject facjure/gardener "0.1.1-SNAPSHOT"
-  :description "A utility belt for Garden: CSS in Clojure/Clojurescript."
-  :url "https://github.com/priyatam/gardener"
+(defproject facjure/gardener "0.2.0-SNAPSHOT"
+  :description "A tiny sdk in Clojurescript."
+  :url "https://github.com/facjure/gardener"
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :min-lein-version "2.5.0"
+
+  :min-lein-version "2.6.0"
   :global-vars {*warn-on-reflection* true}
 
-  :dependencies                  [[org.clojure/clojure "1.7.0" :scope "provided"]
-                                  [org.clojure/clojurescript "1.7.28" :scope "provided"]
-                                  [garden "1.3.0-SNAPSHOT"]]
+  :dependencies
+  [[org.clojure/clojure "1.8.0" :scope "provided"]
+   [org.clojure/clojurescript "1.8.51" :scope "provided"]]
 
   :npm
-  {:dependencies                 [[source-map-support "0.3.1"]]}
+  {:dependencies [[source-map-support "0.3.1"]]}
 
   :clean-targets
-  ^{:protect false}              ["target"]
+  ^{:protect false} ["target"]
 
   :profiles
-  {:dev {:source-paths           ["src" "test" "dev"]
-         :dependencies           [[criterium "0.4.1"]]
-         :plugins                 [[codox "0.8.13"]
-                                   [lein-npm "0.6.1"]
-                                   [com.jakemccrary/lein-test-refresh "0.10.0"]]}}
+  {:dev {:source-paths ["src" "test" "dev"]
+         :dependencies [[criterium "0.4.1"]]
+         :plugins [[lein-npm "0.6.2"]]}}
 
   :aliases
-  {"build-cljs"                  ["run" "-m" "user/build"]
-   "node-repl"                   ["run" "-m" "user/node-repl"]
-   "test-clj"                    ["do" "clean," "run" "-m" "user/build," "test-refresh"]
-   "test-cljs"                   ["run" "-m" "gardener.tests/test-all"]})
+  {"build-cljs" ["run" "-m" "user/build"]
+   "node-repl" ["run" "-m" "user/node-repl"]
+   "test-clj" ["do" "clean," "run" "-m" "user/build," "test-refresh"]
+   "test-cljs" ["run" "-m" "gardener.tests/test-all"]})
