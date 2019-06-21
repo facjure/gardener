56bd3c7700270c5d58b10c7100c725b61823da76
Priyatam Mudivarti <priyatam@facjure.com>
2019-06-21 13:41:55 -0700

    add back button api

--- src/gardener/browser/history.cljs
+++ src/gardener/browser/history.cljs
@@ -15,3 +15,22 @@
     (swap! history pop)
     (reset! app-state (last @history))))
 
+
+(defn prevent-browser-back!
+  "Disable the back button: user doesn't navigate away from page"
+  []
+  (.pushState js/history nil nil (.-pathname (.-location js/window))))
+
+(defn alert-after-attempted-refresh! []
+  (set! (.-onbeforeunload js/window) (fn [_] true)))
+
+(defn popstate-mod! []
+  (set! (.-onpopstate js/window)
+        (fn [_] (prevent-browser-back!))))
+
+(defn scroll-to-elem
+  "Enable the page to scroll to a element with a `target-id`"
+  [target-id]
+  (let [elem (.getElementById js/document target-id)]
+    (.scrollIntoView elem #js{:behavior "smooth"})))
+
